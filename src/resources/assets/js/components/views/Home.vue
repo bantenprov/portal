891e69906f01ede3508b0e6562a163b5948b985f
badueny <badueny@gmail.com>
2018-03-27 03:07:36 +0700

    Second Update

--- src/resources/assets/js/components/views/Home.vue
+++ src/resources/assets/js/components/views/Home.vue
@@ -1,20 +1,26 @@
 <template>
-	<div>
-    <div class="home-header header-bgimage">
+ <div>
+    <div class="home-header header-bgimage" style="margin-bottom: 0px;">
+      <nav class="home-header-navbar navbar navbar-expand-lg navbar-light bg-light border-bottom border-info">
 
-      <nav class="home-header-navbar navbar navbar-expand-lg navbar-dark bg-transparent">
+
         <div class="container">
           <router-link class="navbar-brand d-flex flex-row align-items-center text-uppercase" :to="{ name: 'home' }" exact>
             <img class="mr-2" src="/images/logo.png" width="36" height="36">
             <span>Portal Banten</span>
-          </router-link>
+          </router-link> 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav ml-auto">
-               <li class="nav-item"><a   class="nav-link" href="#asas"">Layanan</a></li>
-               
+          <scrollactive class="nav navbar-nav ml-auto"  active-class="active" :offset="80" :duration="800" bezier-easing-value=".5,0,.35,1">
+        
+          <ul class="nav navbar-nav ml-auto">
+               <li class="nav-item"><a  class="nav-link scrollactive-item" href="#beranda">Beranda</a></li>
+               <li class="nav-item"><a   class="nav-link scrollactive-item" href="#asas">Layanan</a></li>
+               <li class="nav-item"><a   class="nav-link scrollactive-item" href="#anggaran">Anggaran</a></li>
+               <li class="nav-item"><a   class="nav-link scrollactive-item" href="#tentang">Tentang</a></li> 
+                           
               <li v-if="authenticated" class="nav-item">
                 <a @click.prevent="logout" class="nav-link" href="#">
                   <i class="fa fa-sign-out fa-fw" aria-hidden="true"></i> Logout
@@ -24,61 +30,102 @@
                 <li class="nav-item">
                   <router-link class="nav-link" :to="{ name: 'login' }"><i class="fa fa-sign-in" aria-hidden="true"></i> Log In</router-link>
                 </li>
-              </template>
+              </template>             
             </ul>
+            </scrollactive>           
           </div>
         </div><!-- /.container -->
       </nav><!-- /.navbar -->
 
-      <div class="container">
+      <div id="beranda" class="container">
         <div class="home-header-content d-flex flex-column justify-content-center py-5">
           <div>
-            <h1 class="home-header-title">Portal <strong class="text-warning"> Banten</strong></h1>
-          <p class="home-header-description mb-5 wow fadeInUp">Gerbang Informasi dan Layanan<br>Pemerintah Provinsi Banten.</p>
-            <p class="home-header-description mb-5">Cari layanan / informasi yang anda inginkan:<br>
-            <div class="btn btn-lg btn-outline-light d-block d-sm-inline-block px-5 mx-2 mb-3 mb-sm-0 animated zoomIn" deadline="June 01, 2018"></div>
-            </p>
+             <h1 class="home-header-title">Portal <strong class="text-warning"> Banten</strong></h1>
+          <p class="home-header-description mb-5 wow fadeInUp">Gerbang Informasi dan Layanan<br>Pemerintah Provinsi Banten.</p>            
+            <div class="home-header-description mb-5 input-group justify-content-center py-5 wow slideInUp">       
+                <input type="text" class="form-control rounded-left col-8" placeholder="Cari layanan / informasi yang anda inginkan..." aria-label="" aria-describedby="basic-addon1">                
+                <div class="input-group-prepend">
+                  <router-link class="btn btn-tanara rounded-right " :to="{ name: 'home' }"><i class="fa fa-search"></i> Cari</router-link>
+                </div>  
+            </div> 
             <div class="clearfix"></div>
             <p class="mb-0 wow fadeInDown">
-              <router-link class="btn btn-lg btn-light d-block d-sm-inline-block px-5 mx-2 mb-3 mb-sm-0 animated slideInLeft" :to="{ name: 'dashboard' }"><i class="fa fa-th-large" aria-hidden="true"></i> Dashboard</router-link>
-              <router-link v-if="!authenticated" class="btn btn-lg btn-outline-light d-block d-sm-inline-block px-5 mx-2 mb-3 mb-sm-0 animated slideInRight" :to="{ name: 'register' }"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> Daftar</router-link>
+              <router-link class="btn btn-lg btn-light d-block d-sm-inline-block px-5 mx-2 mb-3 mb-sm-0 animated slideInLeft" :to="{ name: 'dashboard' }"><i class="fa fa-th-large" aria-hidden="true"></i> Dashboard</router-link>              
             </p>
           </div>
         </div>
-      </div><!-- /.container -->
+      </div><!-- /.container -->      
+    </div><!-- /.banner -->
 
-      
-	  </div><!-- /.banner -->
-
-		<section id="asas" class="home-section home-section-1a">
-	    <div class="container">
+    <section id="asas" class="home-section home-section-1a">
+      <div class="container">
         <h2>Layanan</h2>
         <p class="lead">Pemerintah Provinsi Banten memberikan layanan dalam bentuk sistem informasi / aplikasi yang dapat diakses oleh masyarakat.</p>
         <div class="row">
-          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="200ms"> <i class="fa fa-bullseye ikon"></i>
-            <h4><strong>Banten Satu Login</strong></h4>
-            <p>Selain dipergunakan oleh ASN di lingkup pemerintahan juga dapat digunakan untuk mengakses Layanan Pemerintah Provinsi Banten oleh masyarakat.</p>
+          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="mumbul-box brand-bg text-center z-depth-1">
+                  <div class="mumbul-wrapper">
+                    <div class="intro-header">
+                      <i class="fa fa-sign-in ikon"></i>
+                      <h4 class="service-title">Banten Satu Login</a></strong></h4>
+                    </div>
+                    <div class="content white-text">
+                      <p>Satu tempat untuk mengakses Aplikasi Layanan Pemerintah Provinsi Banten bagi ASN dan Masyarakat.<br>                      
+                      <router-link class="btn btn-tanara rounded" :to="{ name: 'home' }"><i class="fa fa-external-link"></i> Kunjungi</router-link></p> 
+                    </div>
+                  </div>
+                </div>      
           </div>
-          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="400ms"> <i class="fa fa-search ikon"></i>
-            <h4><strong>Banten Satu Data</strong></h4>
-            <p>Guna mewujudkan sistem informasi dan aplikasi yang terintegrasi, Pemerintah Provinsi Banten menyiapkan basis data tunggal.</p>
+          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="400ms">
+                <div class="mumbul-box brand-bg text-center z-depth-1">
+                  <div class="mumbul-wrapper">
+                    <div class="intro-header">
+                      <i class="fa fa-database ikon"></i>
+                      <h4 class="service-title">Banten Satu Data</a></strong></h4>
+                    </div>
+                    <div class="content white-text">
+                      <p>Basis data tunggal Guna mewujudkan sistem informasi dan aplikasi Pemerintah Provinsi Banten yang terintegrasi.<br>                      
+                      <router-link class="btn btn-tanara rounded" :to="{ name: 'home' }"><i class="fa fa-external-link"></i> Kunjungi</router-link></p>               
+                    </div>
+                  </div>
+                </div>             
           </div>
-          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="600ms"> <i class="fa fa-check-square ikon"></i>
-            <h4><strong>Pelaporan Elektronik</strong></h4>
-            <p>Mendekatkan dengan masyarakat dalam hal menampung aspirasi maupun laporan, Epormas dipersiapkan untuk memudahkan penyampaian laporan.</p>
+          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="600ms"> 
+                <div class="mumbul-box brand-bg text-center z-depth-1">
+                  <div class="mumbul-wrapper">
+                    <div class="intro-header">
+                      <i class="fa fa-comments ikon"></i>
+                      <h4 class="service-title">Pelaporan Elektronik</a></strong></h4>
+                    </div>
+                    <div class="content white-text">
+                      <p>Layanan penyampaian laporan dan Aspirasi untuk menampung aspirasi maupun laporan masyarakat.<br>                      
+                      <router-link class="btn btn-tanara rounded" :to="{ name: 'home' }"><i class="fa fa-external-link"></i> Kunjungi</router-link></p> 
+                    </div>
+                  </div>
+                </div> 
           </div>
-          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="800ms"> <i class="fa fa-balance-scale ikon"></i>
-            <h4><strong>Layanan Kesehatan</strong></h4>
-            <p>Dapat memberikan informasi secara cepat, tepat dan terpadu bagi masyarakat yang membutuhkan informasi layanan kesehatan.</p>
+          <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="800ms"> 
+                <div class="mumbul-box brand-bg text-center z-depth-1">
+                  <div class="mumbul-wrapper">
+                    <div class="intro-header">
+                      <i class="fa fa-medkit ikon"></i>
+                      <h4 class="service-title">Layanan Kesehatan</a></strong></h4>
+                    </div>
+                    <div class="content white-text">
+                      <p>Dapat memberikan informasi secara cepat, tepat dan terpadu bagi masyarakat yang membutuhkan informasi layanan kesehatan.<br>                      
+                      <router-link class="btn btn-tanara rounded" :to="{ name: 'home' }"><i class="fa fa-external-link"></i> Kunjungi</router-link></p> 
+                    </div>
+                  </div>
+                </div> 
           </div>
         </div>        
-	    </div>      
-		</section>
+      </div>      
+    </section>
 
-		<section class="home-section home-section-2a">
-	    <div class="container">
+    <section id="tentang" class="home-section home-section-2">
+      <div class="container">
          <div class="section-title text-center wow fadeInDown">
-              <h4>Tentang <strong>Portal</strong> Banten</h4>
+              <h2>Portal  <strong class="text-warning">Banten</strong></h2>
               <div class="clearfix"></div>
               <p >Portal Pemerintah Provinsi Banten, merupakan pintu gerbang bagi masyarakat untuk mengetahui hasil pembangunan yang telah dilaksanakan yang informasinya secara detil dapat dilihat pada situs situs terkait juga dapat menemukan navigasi atau petunjuk tentang layanan yang diselenggarakan Pemerintah Provinsi Banten.</p>
             </div>
@@ -110,13 +157,13 @@
                     </div>
                   </div>     
               </div>
-            </div>	      
+            </div>        
       </div>
      
-		</section>
+    </section>
 
-		<section class="home-section home-section-3">
-	    <div class="container">
+    <section id="anggaran" class="home-section home-section-3">
+      <div class="container">
         <h2>Anggaran dan Penyerapan</h2>
         <div class="row">
           <div class="col-sm-3 mb-3">
@@ -152,25 +199,25 @@
             </div>
           </div><!-- /col -->
         </div><!-- /.row -->
-	    </div>
+      </div>
     </section>
 
-		<footer class="footer foothome">
+    <footer class="footer foothome">
       <div class="container mt-5">
-        <div class="row">
+        <div class="row col-12">
           <div class="col-8">
-            <h4 class="footer-title text-dark">Peta situs</h4>
-            <ul class="footer-nav text-dark">
+            <h4 class="footer-title text-white">Peta situs</h4>
+            <ul class="footer-nav">
               <li class="footer-nav-item"><a href="#" class="footer-nav-link">Layanan</a></li>
-              <li class="footer-nav-item"><a href="#" class="footer-nav-link">Produk Hukum</a></li>
+              <li class="footer-nav-item "><a href="#" class="footer-nav-link">Produk Hukum</a></li>
               <li class="footer-nav-item"><a href="#" class="footer-nav-link">Anggaran dan Penyerapan</a></li>
               <li class="footer-nav-item"><a href="#" class="footer-nav-link">Informasi Publik</a></li>
               <li class="footer-nav-item"><a href="#" class="footer-nav-link">Perijinan Satu Pintu</a></li>
             </ul>
           </div>
           <div class="col-4">
-              <h4 class="footer-title text-dark">Pemerintah Provinsi Banten</h4>
-              <p>Kawasan Pusat Pemerintahan Provinsi Banten<br>JL. Syech Nawawi Al-Bantani - Curug - Palima<br>Kota Serang - Provinsi Banten<br>Telp/Fax : (0254)  267064 / 267065.</p>
+              <h4 class="footer-title text-white">Pemerintah Provinsi Banten</h4>
+              <p>Kawasan Pusat Pemerintahan Provinsi Banten<br>JL. Syech Nawawi Al-Bantani - Curug - Palima<br>Kota Serang - Provinsi Banten</p>
           </div>
         </div>        
       </div><!-- /.container-fluid -->
@@ -184,11 +231,12 @@
       </div><!-- /.container-fluid -->
     </footer>
 
-	</div>
+  </div>
 </template>
 <style scoped>
+
 .header-bgimage {
-  background: url('/images/banner-ppdb-2.jpg') no-repeat bottom center;
+  background: url('/images/banner-portal-2.jpg') no-repeat bottom center;
   background-attachment: fixed !important; 
   background-size: cover;
 }
@@ -196,10 +244,18 @@
   font-size: 30px;
   padding: 5px;
   color: #7891a3;
+  padding-bottom:2px;
 }
+
+.img-icon {
+  height:49px;
+  width:auto;
+  padding-bottom:10px;
+}
+
 .home-section-1a {
   position: relative;
-  padding-top: 60px;
+  padding-top: 80px;
   padding-bottom: 30px;
   color: #333;
   background-color: #E8EEEF;
@@ -207,10 +263,14 @@
 
 .home-section-2a {
   position: relative;
-  padding-top: 30px;
+  padding-top: 50px;
   padding-bottom: 30px;
   color: #fff;
   background-color: #5B7981;
+  }
+
+  .home-section-2 {  
+  background-color: #3AA18C;
   }
 
   .ratakirikanan {
@@ -240,13 +300,83 @@
   padding-bottom: 5px;
   }
   .foothome{
-    background-color:#278da5;
+    background-color:#298470; /*278da5;*/
     padding-top: 20px;
-  padding-bottom: 20px;
+    padding-bottom: 20px;
   }
+
+  .service-title{
+   color: #298470;
+  }
+
+  .portal-color{
+  color:#298470;
+  }
+  
+  .btn-tanara {
+    color: #fff;
+    background-color: #298470;
+    border-color: #298470;
+  }
+  .btn-tanara:hover {
+    color: #fff;
+    background-color: #FFC107;
+    border-color: #FFC107;
+  }
+
+  .footer-nav-item{
+    color: #fff;
+  }
+
+  #tanarasearch:focus + .input-group-addon {
+    z-index: 10;
+  }
+
+  #search-btn:hover {
+    cursor: pointer;
+    /*color: gold;*/
+    background-color: #ffc107;
+  }
+  .navbar {     
+      padding-top: 0;
+      padding-bottom: 0;
+  }
+
+  .navbar-nav>li.active>a{
+  background:#4fbfa8 !important;
+  color:#fff !important;
+  text-decoration:none !important;
+  border-color:#3aa18c !important;
+  }
+
+  .navbar-nav>li>a{
+  margin:0;
+  padding:1.4rem 1rem 1.6rem;
+  color:#555;font-weight:600;
+  text-transform:uppercase;
+  letter-spacing:0.1em;
+  font-size:0.9rem;
+  text-decoration:none;
+  border-top:.3rem solid transparent;
+  }
+  .navbar-nav>li>a:hover{
+  background:rgba(79,191,168,0.5);
+  border-color:rgba(79,191,168,0.8);
+  }
+  .navbar-nav>li>a:focus{
+  background:#4fbfa8 !important;
+  color:#fff !important;
+  text-decoration:none !important;
+  border-color:#3aa18c !important;
+  }  
+
 </style>
 
 <script>
+
+var VueScrollactive = require('vue-scrollactive');
+Vue.use(VueScrollactive);
+
 import { mapGetters } from 'vuex';
 import Countdown from 'vuejs-countdown';
 
@@ -275,12 +405,14 @@ export default {
       scrollpos = window.scrollY;
 
       if (scrollpos > 78) {
-        header.classList.remove('navbar-dark','bg-transparent');
-        header.classList.add('border-bottom','border-info','navbar-light','fixed-top', 'slideInDown', 'bg-light');
+        //header.classList.remove('navbar-dark','bg-transparent');
+        header.classList.add('fixed-top');
+        //header.classList.add('border-bottom','border-info','navbar-light','fixed-top', 'slideInDown', 'bg-light');
       } else {
-        header.classList.remove('border-bottom','border-info','fixed-top', 'slideInDown', 'navbar-light');
-        header.classList.add('navbar-dark','bg-transparent');
-        header.classList.remove('bg-light');
+          header.classList.remove('fixed-top');
+        //header.classList.remove('border-bottom','border-info','fixed-top', 'slideInDown', 'navbar-light');
+        //header.classList.add('navbar-dark','bg-transparent');
+        //header.classList.remove('bg-light');
       }
     });
   }
